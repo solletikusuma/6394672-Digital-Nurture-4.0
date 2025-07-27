cd5dfb0d9de8723ec81589ee5703af0a5f7f5eec
solletikusuma <solletikusuma53@gmail.com>
2025-07-27 15:15:19 +0530

    successfully added

---
+++ WEEK 6-REACT/2.ReactJS-HOL/studentapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+function App() {
+  return (
+    <div className="Container">
+      <Home/>
+      <About/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
